72753178b3ef38654d521918cfab05a48ffb4bf6
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2025-08-15 10:58:55 -0300

    Fix NPM package generation

--- scripts/make-npm-package.sh
+++ scripts/make-npm-package.sh
@@ -6,23 +6,27 @@ VERSION=$1
 shift
 
 if [ -z $VERSION ]; then
-    echo "Usage: $0 <version> [<target dir>]" >&2
-    exit 13
+	echo "Usage: $0 <version> [<target dir>]" >&2
+	exit 13
 fi
 
 TARGET_DIR=$1
 if [ -z $TARGET_DIR ]; then
-    TARGET_DIR=./build/npm-package/
+	TARGET_DIR=./build/npm-package/
 fi
 
 rm -fr $TARGET_DIR 2>/dev/null
 
 mkdir -p $TARGET_DIR
 
+# Generate 1 to 24 skip method contracts
+node scripts/generateAMPSkips.js $(seq -s" " 1 24)
+
 npx hardhat clean
 env COMPILE_MODE=production npx hardhat compile
 
 git archive --format tar HEAD README.md contracts/ | tar xv -C $TARGET_DIR
+cp -r contracts/amps $TARGET_DIR/contracts
 # rm -fR $TARGET_DIR/contracts/mocks/
 
 mkdir $TARGET_DIR/build
@@ -31,7 +35,7 @@ cp -r artifacts/contracts $TARGET_DIR/build
 cp artifacts/build-info/*.json $TARGET_DIR/build/build-info.json
 
 find $TARGET_DIR -name "*.dbg.json" -delete
-sed "s/%%VERSION%%/$VERSION/" npm-package/package.json > "$TARGET_DIR/package.json"
+sed "s/%%VERSION%%/$VERSION/" npm-package/package.json >"$TARGET_DIR/package.json"
 find $TARGET_DIR
 
 echo "
